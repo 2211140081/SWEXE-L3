b1fa4bfaa620e93d47a5ce0e9d0c3d18ff09411f
Yui Nemoto <2211140081j@ed.fuk.kindai.ac.jp>
2024-10-08 06:13:48 +0000

    test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "e8ffe8f4df204e74854beb5b95b83dea.vfs.cloud9.us-east-1.amazonaws.com"
 end
